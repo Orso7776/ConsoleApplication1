6851712538994858a5218461382305eda07b3a06
Timur Smolnikov <tervfjd@gmail.com>
2024-10-01 11:26:48 +0300

    1111

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,56 +1,18 @@
 ﻿#include <iostream>
-
+#include <cstdlib>
+#include <ctime>
 int main() {
-    double distanceAB, distanceBC;
-    double totalWeight;
-    const double tankCapacity = 500;
-    double fuelConsumption;
-    double fuelNeededAB, fuelNeededBC;
-    double totalFuelNeeded;
+    const int SIZE = 20;
+    int numbers[SIZE];
 
-
-    std::cout << "Введите расстояние от пункта А до пункта В (в км): ";
-    std::cin >> distanceAB;
-    std::cout << "Введите расстояние от пункта В до пункта С (в км): ";
-    std::cin >> distanceBC;
-    std::cout << "Введите итоговый вес видеокарт (в кг): ";
-    std::cin >> totalWeight;
-
-    // расход топлива в зависимости от веса
-    if (totalWeight > 1900) {
-        std::cout << "перевес" << std::endl;
-        return 0;
+    for (int i = 0; i < SIZE; ++i) {
+        numbers[i] = std::rand() % 5 + 5;
     }
-    else if (totalWeight > 1400) {
-        fuelConsumption = 10;
+    for (int i = 0; i < SIZE; ++i) {
+        std::cout << numbers[i] << " ";
+        if ((i + 1) % 10 == 0) {
+            std::cout << std::endl;
+        }
     }
-    else if (totalWeight > 900) {
-        fuelConsumption = 8;
-    }
-    else if (totalWeight > 400) {
-        fuelConsumption = 5;
-    }
-    else {
-        fuelConsumption = 2;
-    }
-
-    fuelNeededAB = distanceAB * fuelConsumption;
-    fuelNeededBC = distanceBC * fuelConsumption;
-    totalFuelNeeded = fuelNeededAB + fuelNeededBC;
-
-    if (fuelNeededAB > tankCapacity) {
-        std::cout << "Недостаточно топлива в баке для достижения пункта В." << std::endl;
-        return 0;
-    }
-
-    double fuelInTankAfterAB = tankCapacity - fuelNeededAB;
-
-    if (fuelNeededBC > fuelInTankAfterAB) {
-        std::cout << "Необходимо заправить " << (fuelNeededBC - fuelInTankAfterAB) << " литров соляры в пункте В." << std::endl;
-    }
-    else {
-        std::cout << "Достаточно топлива для поездки без заправки в пункте В." << std::endl;
-    }
-
     return 0;
 }
